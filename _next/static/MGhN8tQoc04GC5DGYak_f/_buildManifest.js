1770dd9682990670407ebc9490188407621e3c5c
Day-be18 <jora-aslanyan@mail.ru>
2025-03-27 04:06:03 +0300

    Updates

--- _next/static/MGhN8tQoc04GC5DGYak_f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4321406d8ae5c2b1.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

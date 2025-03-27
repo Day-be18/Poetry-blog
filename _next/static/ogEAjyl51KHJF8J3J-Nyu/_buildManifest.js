6273c4955de54682f3d9183e2f6b79532f2d8aed
Day-be18 <jora-aslanyan@mail.ru>
2025-03-27 18:45:34 +0300

    Updates

--- _next/static/ogEAjyl51KHJF8J3J-Nyu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9dcc41d5e13311f6.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
